add920fad606a185566ce658178d04cf58ea3bc7
Finn <fluxr3@gmail.com>
2022-05-03 13:25:03 +1200

    made gp calc component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
+import GpCalc from './components/GpCalc';
+import TillCalc from './components/TillCalc';
+import Home from './components/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/gp-calc' element={<GpCalc />} />
+        <Route path='/till-calc' element={<TillCalc />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
